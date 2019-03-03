2612b38e2f80414cbb89383390f0575a6ad20225
DillonMarshall <46968953+DillonMarshall@users.noreply.github.com>
2019-03-03 13:35:20 -0600

    Update 7dtd-APPLY-CONFIG.sh
    Updated wording for more appeal.

--- 7dtd-APPLY-CONFIG.sh
+++ 7dtd-APPLY-CONFIG.sh
@@ -9,10 +9,10 @@
 ./replace.sh $INSTALL_DIR/Data/Prefabs/skyscraper_03.xml downtown "commercial,downtown" Zoning
 ./replace.sh $INSTALL_DIR/Data/Prefabs/skyscraper_04.xml downtown "commercial,downtown" Zoning
 
-./replace.sh $INSTALL_DIR/serverconfig.xml "My Game Host" "SANITYS EDGE . MODDED . CUSTOM 8K MAP . EXPERIMENTAL" ServerName
+./replace.sh $INSTALL_DIR/serverconfig.xml "My Game Host" "SANITY'S EDGE . MODDED . CUSTOM 8K MAP . EXPERIMENTAL" ServerName
 ./replace.sh $INSTALL_DIR/serverconfig.xml "8" "24" ServerMaxPlayerCount
 ./replace.sh $INSTALL_DIR/serverconfig.xml "0" "2" 'name="ServerAdminSlots"'
-./replace.sh $INSTALL_DIR/serverconfig.xml "A 7 Days to Die server" "An experimental heavily-modded 7DTD Server, using a custom RWG-tweaked map, and over 350 new buildings. ServerTools installed. Modlets: Vanilla+, Red Eagle, Just Survive. No client side installation needed." ServerDescription
+./replace.sh $INSTALL_DIR/serverconfig.xml "A 7 Days to Die server" "An extremely modified experimental 7DTD Server, using a custom RWG-tweaked map, and over 350 new buildings. ServerTools installed. Modlets: Vanilla+, Red Eagle, Just Survive. No client side installation nessesary." ServerDescription
 ./replace.sh $INSTALL_DIR/serverconfig.xml "Navezgane" "RWG" GameWorld
 ./replace.sh $INSTALL_DIR/serverconfig.xml "My Game" "Sanitys Edge" GameName
 ./replace.sh $INSTALL_DIR/serverconfig.xml "asdf" "serenity19" 'name="WorldGenSeed"'
@@ -26,4 +26,4 @@
 #./replace.sh $INSTALL_DIR/serverconfig.xml "60" "90" MaxSpawnedZombies
 #./replace.sh $INSTALL_DIR/serverconfig.xml "50" "80" MaxSpawnedAnimals
 ./replace.sh $INSTALL_DIR/serverconfig.xml "true" "false" EACEnabled
-./replace.sh $INSTALL_DIR/serverconfig.xml 'value=""' 'value="This is an experimental server. Do not play here."' ServerLoginConfirmationText
+./replace.sh $INSTALL_DIR/serverconfig.xml 'value=""' 'value="This is an experimental server. Do NOT use it as your everyday server. It is still under heavy development."' ServerLoginConfirmationText
